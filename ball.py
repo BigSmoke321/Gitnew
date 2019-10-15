34358f9a5bd11f0688ec5893e5edc2aa731cea6d
dimAss <dima.gavrilov-shlyapa@mail.ru>
2019-10-15 03:10:10 +0300

    ddfgdgdg

--- ball.py
+++ ball.py
@@ -2,7 +2,7 @@ from tkinter import *
 from random import randrange as rnd, choice
 import time
 root = Tk()
-root.geometry('800x600')
+root.geometry('1920x1200')
 
 
 
@@ -13,15 +13,15 @@ colors = ['red', 'orange', 'yellow', 'green', 'blue', 'brown']
 def new_ball():
     global x, y, r, x2, y2, r2
     c.delete(ALL)
-    x = rnd(100, 700)
-    y = rnd(100, 500)
+    x = rnd(100, 1820)
+    y = rnd(100, 1100)
     r = rnd(30, 50)
-    x2 = rnd(0, 800)
-    y2 = rnd(0, 600)
+    x2 = rnd(0, 1920)
+    y2 = rnd(0, 1200)
     r2 = rnd(1, 100)
     c.create_oval(x-r, y-r, x+r, y+r, fill=choice(colors), width=0)
     c.create_oval(x2-r2, y2-r2, x2+r2, y2+r2, fill=choice(colors), width=0)
-    root.after(800, new_ball)
+    root.after(1000, new_ball)
 
 
 def click(event):
