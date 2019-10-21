2e74c360fcfb73a8bf80ae95bec8466980291a0a
dimAss <dima.gavrilov-shlyapa@mail.ru>
2019-10-21 21:19:43 +0300

    dank game

--- ball.py
+++ ball.py
@@ -1,31 +1,76 @@
 from tkinter import *
 from random import randrange as rnd, choice
-import time             #yeah it's useless thing
+import math
+
+
+global score, antiscore
+score = 0
+antiscore = 0
+
 root = Tk()
-root.geometry('1920x1200')
+root.geometry('1600x900')
 
+label1 = Label(text=str(score), fg="#eee", bg="#333")
+label2 = Label(text=str(antiscore), fg="#eee", bg="#333")
+label1.pack()
+label2.pack()
 
-
-c=Canvas(root, bg='white')
+c = Canvas(root, bg='white')
 c.pack(fill=BOTH, expand=1)
+colors = ['red', 'orange', 'yellow', 'green', 'blue', 'brown', '#99D9EA', '#BE5AA8']
 
-colors = ['red', 'orange', 'yellow', 'green', 'blue', 'brown']
+st = [-1, 1]
+k = choice(st)
+vx = rnd(-5, 6)
+vy = k * math.sqrt(25 - vx**2)
+
+
 def new_ball():
-    global x, y, r, x2, y2, r2
+    global x, y, r, clr
     c.delete(ALL)
-    x = rnd(100, 1820)
-    y = rnd(100, 1100)
+    x = rnd(100, 1500)
+    y = rnd(100, 800)
     r = rnd(30, 50)
-    x2 = rnd(0, 1920)
-    y2 = rnd(0, 1200)
-    r2 = rnd(1, 100)
-    c.create_oval(x-r, y-r, x+r, y+r, fill=choice(colors), width=0)
-    c.create_oval(x2-r2, y2-r2, x2+r2, y2+r2, fill=choice(colors), width=0)
-    root.after(1000, new_ball)
+    clr = choice(colors)
+    c.create_oval(x - r, y - r, x + r, y + r, fill=clr, width=0)
+    root.after(20, move_ball)
+
+
+def move_ball():
+    global vx, vy, x, y, clr
+    if (x <= 10):
+        st = [-1, 1]
+        k = choice(st)
+        vx = rnd(0, 6)
+        vy = k * math.sqrt(25 - vx ** 2)
+    elif (x >= 1590):
+        st = [-1, 1]
+        k = choice(st)
+        vx = rnd(-5, 1)
+        vy = k * math.sqrt(25 - vx ** 2)
+    elif (y <= 10):
+        vx = rnd(-5, 6)
+        vy = math.sqrt(25 - vx ** 2)
+    elif (y >= 890):
+        vx = rnd(-5, 6)
+        vy = (-1) * math.sqrt(25 - vx ** 2)
+
+    x += vx
+    y += vy
+    c.delete(ALL)
+    c.create_oval(x - r, y - r, x + r, y + r, fill=clr, width=0)
+    root.after(20, move_ball)
 
 
 def click(event):
-    print(x, y, r, '    ', x2, y2, r2)
+    global r, score, antiscore
+    if ((event.x - x) ** 2 + (event.y - y) ** 2) <= r ** 2:
+        score += 1
+    else:
+        antiscore += 1
+    label1['text'] = str(score)
+    label2['text'] = str(antiscore)
+
 
 new_ball()
 c.bind('<Button-1>', click)
